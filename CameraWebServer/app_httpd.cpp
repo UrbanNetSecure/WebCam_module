842e3f1be6adad28ae58356e9bb596de06dced32
Seo Yubin <syb0.14.28@gmail.com>
2024-09-09 19:56:30 +0900

    Update app_httpd.cpp

--- CameraWebServer/app_httpd.cpp
+++ CameraWebServer/app_httpd.cpp
@@ -24,10 +24,9 @@
 #include "esp32-hal-log.h"
 #endif
 
-// Face Detection will not work on boards without (or with disabled) PSRAM
+
 #ifdef BOARD_HAS_PSRAM
-// Face Recognition takes upward from 15 seconds per frame on chips other than ESP32S3
-// Makes no sense to have it enabled for them
+
 #if CONFIG_IDF_TARGET_ESP32S3
 #define CONFIG_ESP_FACE_RECOGNITION_ENABLED 1
 #define CONFIG_ESP_FACE_DETECT_ENABLED      1
