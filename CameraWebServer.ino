3764e7e78453f30d13df8cf7bbb908ab6d6f93d7
Seo Yubin <syb0.14.28@gmail.com>
2024-09-10 08:17:25 +0900

    Update CameraWebServer.ino

--- CameraWebServer.ino
+++ CameraWebServer.ino
@@ -1,127 +1,105 @@
-#include "esp_camera.h"
 #include <WiFi.h>
+#include <WebServer.h>
 
-#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+const char* ssid = "****";
+const char* password = "****$";
 
-#include "camera_pins.h"
+WebServer server(80);
 
+//blocked IP list
 
-// Enter your WiFi credentials
-const char *ssid = "********";
-const char *password = "********";
+const int MAX_BLOCKED_IPS = 10;
+String blockedIPs [MAX_BLOCKED_IPS];
+int blockedIPCount = 0;
 
-void startCameraServer();
-void setupLedFlash(int pin);
+// functions to save logs
+void logAccess(String clientIP, String url, String method, String status) {
+  Serial.print("접속 IP: ");
+  Serial.println(clientIP);
+  Serial.print("오청 URL: ");
+  Serial.println(url);
+  Serial.print("요청 메서드: ");
+  Serial.println(method);
+  Serial.print("상태: ");
+  Serial.println(status);
+  // 외부 서버로 로그 전송
+}
 
-void setup() {
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
-
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sccb_sda = SIOD_GPIO_NUM;
-  config.pin_sccb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
-  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
-  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
-  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
-  config.fb_location = CAMERA_FB_IN_PSRAM;
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
-
-  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    if (psramFound()) {
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    } else {
-      // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  } else {
-    // Best option for face detection/recognition
-    config.frame_size = FRAMESIZE_240X240;
-#if CONFIG_IDF_TARGET_ESP32S3
-    config.fb_count = 2;
-#endif
+// IP 차단 함수
+void blockIP(String ip) {
+  if (blockedIPCount < MAX_BLOCKED_IPS) {
+    blockedIPs [blockedIPCount++] = ip;
+    Serial.print("차단된 IP: ");
+    Serial.println(ip);
   }
+}
 
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
+// IP 차단 체크 함수
+bool isIPBlocked(String ip) {
+  for (int i = 0; i < blockedIPCount; i++) {
+    if (blockedIPs[i] == ip) {
+      return true;
+    }
+  }
+  return false;
+}
 
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
+void handleLogin() {
+  String clientIP = server.client().remoteIP().toString();
+
+  // IP가 차단된 경우 로그인 시도 차단
+  if (isIPBlocked(clientIP)) {
+    logAccess(clientIP, server.uri(), server.method() == HTTP_GET ? "GET" : "POST", "IP BLOCKED");
+    server.send(403, "text/plain", "차단된 IP에서의 접근입니다");
     return;
   }
 
-  sensor_t *s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1);        // flip it back
-    s->set_brightness(s, 1);   // up the brightness just a bit
-    s->set_saturation(s, -2);  // lower the saturation
+  logAccess(clientIP, server.uri(), server.method() == HTTP_GET ? "GET" : "POST", "로그인 시도");
+
+  if (server.hasArg("username") && server.hasArg("password")) {
+    String username = server.arg("username");
+    String password = server.arg("password");
+
+    // simple auth logic
+    if(username == "admin" && password == "password") {
+      server.send(200, "text/plain", "Login Success");
+      //로그인 성공시 차단된 IP 목록 초기화 또는 다른 처리
+    }
+    else {
+      server.send(403, "text/plain", "로그인 실패");
+      blockIP(clientIP); // 로그인 실패가 일정 횟수 초과 시 IP 추가 가능
+    }
   }
-  // drop down frame size for higher initial frame rate
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    s->set_framesize(s, FRAMESIZE_QVGA);
+  else {
+    server.send(400, "text/plain", "필요한 정보가 없습니다");
   }
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
-
-#if defined(CAMERA_MODEL_ESP32S3_EYE)
-  s->set_vflip(s, 1);
-#endif
-
-// Setup LED FLash if LED pin is defined in camera_pins.h
-#if defined(LED_GPIO_NUM)
-  setupLedFlash(LED_GPIO_NUM);
-#endif
+}
+void setup() {
+  Serial.begin(115200);
 
   WiFi.begin(ssid, password);
-  WiFi.setSleep(false);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("WiFi 연결됨");
 
-  startCameraServer();
+  // root page
+  server.on("/", []() {
+    server.send(200, "text/plain", "<h2>web server running</h2><p><a href='/login'>move to login page </a></p>");
+  });
 
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
+  //login page ex
+  server.on("/login", HTTP_POST, handleLogin);
+
+  server.begin();
+  Serial.println("웹 서버 시작");
+
+  //print ip addr
+  Serial.print("웹 서버 주소: http://");
+  Serial.println(WiFi.localIP());
 }
-
 void loop() {
-  // Do nothing. Everything is done in another task by the web server
-  delay(10000);
+  server.handleClient();
 }
